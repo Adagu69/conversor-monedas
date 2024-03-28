6a8ce8524b51beeeec45bc2a9d9ee22ef35f9a56
Adriel <adrielgudielurquizo@gmail.com>
2024-03-28 18:11:02 -0500

    3-Ultimo ajuste

--- logic.js
+++ logic.js
@@ -5,5 +5,7 @@ var cotizacionEnPesos = 16.895;
 var valorEnPesos = valorEnDolar * cotizacionEnPesos;
 
 valorEnPesos = valorEnPesos.toFixed(2);
+console.log("El valor de " + valorEnDolar + " dólares en pesos es: $" + valorEnPesos);
+
 
 alert("$" + valorEnPesos);
